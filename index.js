4308f9b7455b21c12329a1624fac3309b9ba055f
Matheus O magnata <matheuslv.16ninja@gmail.com>
2021-01-24 21:48:11 -0400

    Host

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
+
 const config = require('./config.json')
+const command = require('./command.js')
 
 client.on('ready', () => {
-    console.log('UwU?')
+    console.log('OwO?')
+
+    command(client, ['OwO'], (message) => {
+        message.channel.send('OwO?')
+    })
+
+    command(client, ['help', 'comandos'], (message) => {
+        message.channel.send('comandos by Balthasar-2 0.0.13')
+    })
+
+    command(client, ['ping', 'test'], (message) => {
+        message.channel.send('Pong!')
+    })
 })
 
 client.login(config.token)
